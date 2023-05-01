ca32c077bf9a90200b7e6e6dbe959488a8551ef9
Dima-Ivanov <iv200231@gmail.com>
2023-05-01 19:33:47 +0300

    added home page description

--- share-class-react/src/components/home/home.js
+++ share-class-react/src/components/home/home.js
@@ -7,7 +7,20 @@ const Home = ({ setHeaderPlusButton }) => {
 
   return (
     <>
-      <h3>Home</h3>
+      <h1>Home</h1>
+      <div style={{ fontSize: "large" }}>
+        ShareClass is a platform that provides an alternative to Classroom
+        Google, where the main goal is to facilitate the exchange of homework
+        solutions between students. The users are able to create class-rooms and
+        manage them, add homework assignments with a title/description/due date,
+        and publish/view solutions to the homework assignments. <br />
+        The class-rooms can be joined by invitation only. Only the
+        administrators, who are the creators of the class-rooms, are able to add
+        homework assignments, while all members of the class-room can add
+        solutions to the homework assignments.
+        <br /> <br />
+        Current version: 1.0
+      </div>
     </>
   );
 };
